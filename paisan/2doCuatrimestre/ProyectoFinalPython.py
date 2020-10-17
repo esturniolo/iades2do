ea9d5626dee1c2392198b005c5d360530796a5b0
Emiliano <emiliano@sturniolo.com.ar>
2020-10-17 20:54:56 -0300

    removeUser() still in progress

--- paisan/2doCuatrimestre/ProyectoFinalPython.py
+++ paisan/2doCuatrimestre/ProyectoFinalPython.py
@@ -1,69 +1,44 @@
 
 def newUser():
-    addEmployee= open("infoEmpleados.txt", "a")
+    f = open("infoEmpleados.txt", "a")
     idEmployee= input("Ingrese el numero de legajo del empleado: ")
     nameEmployee= input("Ingrese el nombre del empleado: ")
     salaryEmployee= input("Ingrese el salario bruto del empleado: ")
     data = idEmployee+nameEmployee.center(20)+"$"+salaryEmployee.center(10)+"\n"
-    addEmployee.write(str(data))
-    addEmployee.close()
+    f.write(str(data))
+    f.close()
 
 def removeUser():
-    removeEmployee= open("infoEmpleados.txt", "r+")
-    listEmployeeId= removeEmployee.readlines()
+    f = open("infoEmpleados.txt", "r+")
+    listEmployeeId= f.readlines()
     employeeId= input(str("Ingrese el numero de legajo del empleado a dar de baja: "))
     warning= print("\nUsted ingresó el legajo", employeeId,". Está seguro de dar de baja este legajo? \nESTA ACCION NO SE PUEDE DESHACER")
     confirmation = input("S/N\n")
     if ( confirmation == "S"):
-        # listEmployeeId= removeEmployee.readlines()
-        # print (listEmployeeId)
-        counter=0
+        
         idRemoved = 0
         for line in listEmployeeId:
             result = (line[:3])
             if (result == employeeId):
-                listEmployeeId.pop(counter)
+            # if result!=employeeId:
+                # f.seek(0)
+                listEmployeeId.remove(line)
+                # f.write(line)
                 idRemoved = 1
-            counter = counter + 1
+            elif result!=employeeId:
+                f.write(line)
         if (idRemoved == 1):
             print("El legajo",employeeId,"fue eliminado satisfactoriamente.")
         else:
             print("El legajo",employeeId,"no existe.")
     for line in listEmployeeId:
         print (line)
-    removeEmployee.close()
+    f.close()
     # elif (estaSeguro.upper() == "N"):
 
 
 removeUser()
 
-# removeEmployee= open("infoEmpleados.txt", "r+")
-# listEmployeeId= removeEmployee.readlines()
-# # print(listEmployeeId)
-
-# print(listEmployeeId)
-
-# a = str(111)
-# contador=0
-
-# for linea in listEmployeeId:
-#     # print(linea)
-#     resultado = (linea[:3])
-#     if (resultado == a):
-#         # print ("ok")
-#         listEmployeeId.pop(contador)
-#     # else:
-#         # print ("notok")
-#     contador = contador + 1
-
-# print(listEmployeeId)
-# removeEmployee.close()
-
-# # a=111
-
-# # for a in listEmployeeId
-
-# # ['elementOne', 'elementThree']
-# # >>> stringlist = [x for x in stringlist if "Two" not in x]
-# # >>> stringlist
-# # ['elementOne', 'elementThree']
+# for line in lines:
+#      if line!=result+"\n":
+#        f.write(line)
